be871ef4f7ff815eaf7213c18ee6a37db828095f
Radmyr Deriabin <radmyr.deriabin11@gmail.com>
2023-04-19 21:21:59 +0200

    helmet added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import Home from './components/Home'
 import About from './components/About';
 import WorksSlider from './components/WorksSlider';
 import Contact from './components/Contact';
+import { Helmet } from 'react-helmet';
+import Photo1 from '../assets/1.jpg';
 
 function App() {
   return (
     <>
+    <Helmet>
+        <title>Molfar.cake</title>
+        <meta name="description" content="A confectioner's website offering handmade, artisanal sweets and baked goods" />
+        <meta property="og:title" content="Molfare.cake" />
+        <meta property="og:description" content="A confectioner's website offering handmade, artisanal sweets and baked goods" />
+        <meta property="og:image" content={Photo1} />
+      </Helmet>
       <Navbar />
       <Home />
       <About />
